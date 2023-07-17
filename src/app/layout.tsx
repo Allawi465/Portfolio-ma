4a803fdd54099068652220ed370b6352453329e0
Allawi <allawi465@gmail.com>
2023-07-17 16:13:35 +0200

    more content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,9 @@
 'use client';
 import './style/globals.css';
 import { useState, useEffect } from 'react';
-import type { Metadata } from 'next';
 import Navbar from './components/layout/navbar';
 import Footer from './components/layout/footer';
 import Loading from './loading';
-
-export const metadata: Metadata = {
-  title: 'Portfolio | MA',
-  description: 'My personal portfolio',
-  keywords: 'front-end developer, next.js, typescript, tailwind, thereJs',
-};
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import type { Metadata } from 'next';
 import Introduction from './components/hero';
 import AboutMe from './components/about';
 import Projects from './components/projects/index';
+
+export const metadata: Metadata = {
+  title: 'Portfolio | MA',
+  description: 'My personal portfolio',
+  keywords: 'front-end developer, next.js, typescript, tailwind, thereJs',
+};
 
 export default function Home() {
   return (
