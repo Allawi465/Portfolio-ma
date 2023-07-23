371f258f99ca72bc8845f297db7dcf3f361eec1e
Allawi <allawi465@gmail.com>
2023-07-24 00:18:58 +0200

    more content

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 const withNextIntl = require('next-intl/plugin')('./i18n.ts');
+
 module.exports = withNextIntl({
+  output: {
+    export: true, // Set the output type to "export"
+  },
   rewrites() {
     return [
       {
